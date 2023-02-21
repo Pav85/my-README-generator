8d851ce465a5b48fbd003dd9144090ae45929a92
Pav85 <pawel.werbowy@gmail.com>
2023-02-21 08:39:07 +0000

    adds few questions to start

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "project_name",
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project",
+    name: "description",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
